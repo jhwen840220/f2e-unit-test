250b7d6ed2f724d6eb073d1c4118214536df90cc
jhwen840220 <jhwen840220@gmail.com>
2020-06-12 16:50:58 +0800

    test("Input:42, target=42")

--- src/008.String-to-Integer/string-to-integer.js
+++ src/008.String-to-Integer/string-to-integer.js
@@ -3,6 +3,10 @@
  * @return {number}
  */
 const myAtoi = function(str) {
-
+    let arrVal = str.split('').reverse();
+    return arrVal.reduce((result, number, index)=> {
+      result += number * Math.pow(10, index);
+      return result;
+    }, 0)
 };
 export default myAtoi;
