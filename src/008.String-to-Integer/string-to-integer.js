e2c9482b29f97581b620fce4cdd157fd8f7b5310
jhwen840220 <jhwen840220@gmail.com>
2020-06-15 15:06:36 +0800

    正向 loop

--- src/008.String-to-Integer/string-to-integer.js
+++ src/008.String-to-Integer/string-to-integer.js
@@ -5,13 +5,13 @@
 const myAtoi = function(str) {
     const matchValArr = str.trim().match(/^(\+|-)?(\d+)/);
     const sign = matchValArr && matchValArr[1] === "-" ? -1 : 1;
-    const numberVal = matchValArr ? matchValArr[2].split('').reverse() : [];
+    const numberVal = matchValArr ? matchValArr[2].split('') : [];
     const max = Math.pow(2, 31) - 1;
     const min = -Math.pow(2, 31);
 
-    const result = sign * numberVal.reduce((pre, strVal, index) => {
-        pre += strVal * Math.pow(10, index);
-        return pre;
+    const result = sign * numberVal.reduce((pre, strVal) => {
+        pre = pre * 10 + strVal * 1;
+        return pre
     }, 0);
 
     if (result > max) return max;
