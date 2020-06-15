428e91b2d1688aafd3244005b21820defed41609
jhwen840220 <jhwen840220@gmail.com>
2020-06-15 14:42:05 +0800

    修改 + - 號 result

--- src/008.String-to-Integer/string-to-integer.js
+++ src/008.String-to-Integer/string-to-integer.js
@@ -3,21 +3,20 @@
  * @return {number}
  */
 const myAtoi = function(str) {
-    const matchVal = str.trim().match(/^(\+|-)?\d+/);
-    const arrVal = matchVal ? matchVal[0].split('').reverse() : [];
+    const matchValArr = str.trim().match(/^(\+|-)?(\d+)/);
+    const sign = matchValArr && matchValArr[1] === "-" ? -1 : 1;
+    const numberVal = matchValArr ? matchValArr[2].split('').reverse() : [];
     const max = Math.pow(2, 31) - 1;
     const min = -Math.pow(2, 31);
 
-    const result = arrVal.reduce((pre, strVal, index) => {
-        if (strVal === "-") return -pre;
-        else if (strVal === "+") return pre;
+    const result = sign * numberVal.reduce((pre, strVal, index) => {
         pre += strVal * Math.pow(10, index);
         return pre;
-
     }, 0);
 
     if (result > max) return max;
     else if (result < min) return min;
     else return result;
 };
+
 export default myAtoi;
