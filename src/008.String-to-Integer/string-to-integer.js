cca28e62743bfbd02870e5e0a333a6cdd57d1cc1
jhwen840220 <jhwen840220@gmail.com>
2020-06-15 15:28:10 +0800

    reduce

--- src/008.String-to-Integer/string-to-integer.js
+++ src/008.String-to-Integer/string-to-integer.js
@@ -5,11 +5,14 @@
 const myAtoi = function(str) {
     const matchValArr = str.trim().match(/^(\+|-)?(\d+)/);
     const sign = matchValArr && matchValArr[1] === "-" ? -1 : 1;
-    const numberVal = matchValArr ? matchValArr[2] : '';
+    const numberVal = matchValArr ? matchValArr[2].split('') : [];
     const max = Math.pow(2, 31) - 1;
     const min = -Math.pow(2, 31);
 
-    const result = sign * numberVal;
+    const result = sign * numberVal.reduce((pre, strVal) => {
+        pre = pre * 10 + strVal * 1;
+        return pre
+    }, 0);
 
     if (result > max) return max;
     else if (result < min) return min;
